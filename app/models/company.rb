e5ac3a3243fa5182280c5580dd302a06acc1260b
Alefe Chorna <alefechorna@Alefes-MacBook-Pro.local>
2025-05-15 22:47:24 -0300

    feat: add validations for entity registration type and number in Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,6 +6,20 @@ class Company < ApplicationRecord
   has_many :subsidiaries, class_name: 'Company', foreign_key: 'parent_id', dependent: :nullify
   
   validates :trader_name, presence: true
+  validate :entity_registration_type_matches
+  validate :entity_registration_number
 
   accepts_nested_attributes_for :entity
+
+  private
+
+  def entity_registration_type_matches
+    return if entity.registration_type == 'cnpj'
+    errors.add(:registration_type, 'must be cnpj')
+  end
+
+  def entity_registration_number
+    return if entity.registration_number.length == 14
+    errors.add(:base, 'registration_number must be 14 characters')
+  end
 end
