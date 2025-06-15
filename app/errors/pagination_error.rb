621b89dbd27053a28686833768f912fc9442c1f4
Alefe Chorna <alefechorna@gmail.com>
2025-06-15 20:34:05 -0300

    fix: update PaginationError initialization to remove keyword argument for message

--- app/errors/pagination_error.rb
+++ app/errors/pagination_error.rb
@@ -1,6 +1,6 @@
 # app/errors/pagination_error.rb
 class PaginationError < ApplicationError
-  def initialize(message: "")
+  def initialize(message)
     super(
       status: :unprocessable_entity,
       code: "PaginationError",
@@ -9,10 +9,10 @@ class PaginationError < ApplicationError
   end
 
   def self.max_per_page_exceeded(max_per_page)
-    new(message: "per_page cannot exceed #{max_per_page}")
+    new("per_page cannot exceed #{max_per_page}")
   end
 
   def self.invalid_page
-    new(message: "page cannot be less than 1")
+    new("page cannot be less than 1")
   end
 end
